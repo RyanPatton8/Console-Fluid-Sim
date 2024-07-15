5599a15ffd9c00c1e59b51645362f3fe83610dd2
Ryan Patton <ryanpatton173@gmail.com>
2024-07-15 15:43:01 -0400

    Finished implementing features

--- Program.cs
+++ Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Reflection;
 using System.Threading;
+using System.Text.RegularExpressions;
 
-
-// break the water into lists of 100
-// loop through each list
 class Program
 {
     //width and height of terminal
@@ -20,94 +18,101 @@ class Program
 
     public static void Simulate()
     {
-        while (true)
+        bool simulating = true;
+
+        while (simulating)
         {   // for every drop of water check adjacent tiles and move accordingly replacing previous placement with a blank tile
             foreach (Water water in waterList){
-                string direction = water.Move(grid[water.positionX, water.positionY + 1],       //below 
-                                              grid[water.positionX - 1, water.positionY],       //left
-                                              grid[water.positionX + 1, water.positionY],       //right
-                                              grid[water.positionX - 1, water.positionY + 1],   //bottom left
-                                              grid[water.positionX + 1, water.positionY + 1]);   //bottom right  
+                try{
+                        string direction = water.Move(grid[water.positionX, water.positionY + 1],       //below 
+                                                grid[water.positionX - 1, water.positionY],             //left
+                                                grid[water.positionX + 1, water.positionY],             //right
+                                                grid[water.positionX - 1, water.positionY + 1],         //bottom left
+                                                grid[water.positionX + 1, water.positionY + 1]);        //bottom right  
 
-                if (direction == "below"){
-                    Console.SetCursorPosition(water.positionX, water.positionY + 1);
-                    Console.Write(water.character);
-                    grid[water.positionX, water.positionY + 1] = water.character;
+                    if (direction == "below"){
+                        Console.SetCursorPosition(water.positionX, water.positionY + 1);
+                        Console.Write(water.character);
+                        grid[water.positionX, water.positionY + 1] = water.character;
 
-                    Console.SetCursorPosition(water.positionX, water.positionY);
-                    Console.Write(' ');
-                    grid[water.positionX, water.positionY] = ' ';
+                        Console.SetCursorPosition(water.positionX, water.positionY);
+                        Console.Write(' ');
+                        grid[water.positionX, water.positionY] = ' ';
 
-                    water.positionY ++;
+                        water.positionY ++;
+                    }
+                    else if (direction == "bottomLeft"){
+                        Console.SetCursorPosition(water.positionX - 1, water.positionY + 1);
+                        Console.Write(water.character);
+                        grid[water.positionX - 1, water.positionY + 1] = water.character;
+
+                        Console.SetCursorPosition(water.positionX, water.positionY);
+                        Console.Write(' ');
+                        grid[water.positionX, water.positionY] = ' ';
+
+                        water.positionX --;
+                        water.positionY ++;
+                    }
+                    else if (direction == "bottomRight"){
+                        Console.SetCursorPosition(water.positionX + 1, water.positionY + 1);
+                        Console.Write(water.character);
+                        grid[water.positionX + 1, water.positionY + 1] = water.character;
+
+                        Console.SetCursorPosition(water.positionX, water.positionY);
+                        Console.Write(' ');
+                        grid[water.positionX, water.positionY] = ' ';
+
+                        water.positionX ++;
+                        water.positionY ++;
+                    }
+                    else if (direction == "left"){
+                        Console.SetCursorPosition(water.positionX - 1, water.positionY);
+                        Console.Write(water.character);
+                        grid[water.positionX - 1, water.positionY] = water.character;
+
+                        Console.SetCursorPosition(water.positionX, water.positionY);
+                        Console.Write(' ');
+                        grid[water.positionX, water.positionY] = ' ';
+
+                        water.positionX --;
+                    }
+                    else if (direction == "right"){
+                        Console.SetCursorPosition(water.positionX + 1, water.positionY);
+                        Console.Write(water.character);
+                        grid[water.positionX + 1, water.positionY] = water.character;
+
+                        Console.SetCursorPosition(water.positionX, water.positionY);
+                        Console.Write(' ');
+                        grid[water.positionX, water.positionY] = ' ';
+
+                        water.positionX ++;
+                    }
+                    if (direction == "above"){
+                        Console.SetCursorPosition(water.positionX, water.positionY - 1);
+                        Console.Write(water.character);
+                        grid[water.positionX, water.positionY - 1] = water.character;
+
+                        Console.SetCursorPosition(water.positionX, water.positionY);
+                        Console.Write('0');
+                        grid[water.positionX, water.positionY] = '0';
+
+                        water.positionY --;
+                    }
                 }
-                else if (direction == "bottomLeft"){
-                    Console.SetCursorPosition(water.positionX - 1, water.positionY + 1);
-                    Console.Write(water.character);
-                    grid[water.positionX - 1, water.positionY + 1] = water.character;
-
-                    Console.SetCursorPosition(water.positionX, water.positionY);
-                    Console.Write(' ');
-                    grid[water.positionX, water.positionY] = ' ';
-
-                    water.positionX --;
-                    water.positionY ++;
-                }
-                else if (direction == "bottomRight"){
-                    Console.SetCursorPosition(water.positionX + 1, water.positionY + 1);
-                    Console.Write(water.character);
-                    grid[water.positionX + 1, water.positionY + 1] = water.character;
-
-                    Console.SetCursorPosition(water.positionX, water.positionY);
-                    Console.Write(' ');
-                    grid[water.positionX, water.positionY] = ' ';
-
-                    water.positionX ++;
-                    water.positionY ++;
-                }
-                else if (direction == "left"){
-                    Console.SetCursorPosition(water.positionX - 1, water.positionY);
-                    Console.Write(water.character);
-                    grid[water.positionX - 1, water.positionY] = water.character;
-
-                    Console.SetCursorPosition(water.positionX, water.positionY);
-                    Console.Write(' ');
-                    grid[water.positionX, water.positionY] = ' ';
-
-                    water.positionX --;
-                }
-                else if (direction == "right"){
-                    Console.SetCursorPosition(water.positionX + 1, water.positionY);
-                    Console.Write(water.character);
-                    grid[water.positionX + 1, water.positionY] = water.character;
-
-                    Console.SetCursorPosition(water.positionX, water.positionY);
-                    Console.Write(' ');
-                    grid[water.positionX, water.positionY] = ' ';
-
-                    water.positionX ++;
-                }
-                if (direction == "above"){
-                    Console.SetCursorPosition(water.positionX, water.positionY - 1);
-                    Console.Write(water.character);
-                    grid[water.positionX, water.positionY - 1] = water.character;
-
-                    Console.SetCursorPosition(water.positionX, water.positionY);
-                    Console.Write('0');
-                    grid[water.positionX, water.positionY] = '0';
-
-                    water.positionY --;
+                catch(ArgumentOutOfRangeException){
+                    simulating = false;
+                    break;
                 }
             }
             //how long to wait until next loop
             Thread.Sleep(flowRate);  
         }
+        Menu();
     }
 
     // set all starting variables accordingly
     static private void SetUp()
     {
-        //ensure cursor is invisible for aesthetic
-        Console.CursorVisible = false;
         //set window height and width to current position
         width = Console.WindowWidth;
         height = Console.WindowHeight;
@@ -121,6 +126,7 @@ class Program
 
         // create a list of instantiated water objects
         waterList.Clear();
+
         for(int i = waterAmount; i > 0; i--){
             Water water = new Water();
             waterList.Add(water);
@@ -133,11 +139,63 @@ class Program
             flowRate = 0;
         }
     }
+    static void Menu()
+    {
+        bool settingUp = true;
+        //ensure cursor is invisible for aesthetic
+        Console.CursorVisible = true;
+        while(settingUp){
+            try{
+                SetUp();
+                scene.Clear(width, height);
+                settingUp = false;
+            }
+            catch(ArgumentOutOfRangeException){
+                continue;
+            }
+        }
+
+        Regex regex = new Regex("[123]");
+
+        string? input = "blank";
+        bool validInput = false;
+
+        Console.WriteLine("Enter a number (1, 2, 3) or type 'exit' to quit:");
+        
+        while(!validInput){
+            input = Console.ReadLine();
+
+            if (input.ToLower() == "exit")
+            {
+                Console.WriteLine("Exiting the program. Goodbye!");
+                Environment.Exit(0);
+            }
+            else if(regex.IsMatch(input)){
+                validInput = true;
+            }
+            else{
+                Console.WriteLine("You MUST Input either 1, 2, 3 or 'exit'!");
+            }
+        }
+
+        SetUp();
+        //ensure cursor is invisible for aesthetic
+        Console.CursorVisible = false;
+
+        if(input == "1"){
+            scene.SceneOne();
+        }
+        else if(input == "2"){
+            scene.SceneTwo();
+        }
+        else if(input == "3"){
+            scene.SceneThree();
+        }
+        Simulate();
+    }
 
     static void Main(string[] args)
     {
-        SetUp();
-        scene.OriginalScene();
-        Simulate();
+        Menu();
     }
 }
--- Scene.cs
+++ Scene.cs
@@ -1,11 +1,58 @@
 class Scene{
-    public void OriginalScene(){
+    public void SceneOne(){
         Shapes.BlankCanvas(Program.width, Program.height);
-        Shapes.SteepSlopeL(60,9);
-        Shapes.SteepSlopeR(36,14);
-        Shapes.SteepSlopeL(60,18);
-        Shapes.GentleSlopeL(65, 30);
-        Shapes.GentleSlopeR(20,40);
-        Shapes.Platform(40, 55);
+        Shapes.SteepSlopeL(60, 6);
+        Shapes.SteepSlopeR(36, 11);
+        Shapes.SteepSlopeL(60, 15);
+        Shapes.GentleSlopeL(65, 27);
+        Shapes.GentleSlopeR(20, 37);
+        Shapes.Platform(40, 52);
+        Shapes.Platform(70, 52);
+        Shapes.GentleSlopeR(100, 52);
+        Shapes.GentleSlopeL(160, 67);
+    }
+
+    public void SceneTwo(){
+        Shapes.BlankCanvas(Program.width, Program.height);
+        Shapes.Platform(40, 8);
+        Shapes.Platform(70, 8);
+        Shapes.Platform(90, 15);
+        Shapes.Platform(80, 30);
+        Shapes.Platform(115, 30);
+        Shapes.GentleSlopeR(30,20);
+        Shapes.GentleSlopeR(45,35);
+        Shapes.GentleSlopeR(60,50);
+        Shapes.Platform(70, 70);
+        Shapes.Platform(100, 70);
+    }
+
+    public void SceneThree(){
+        Shapes.BlankCanvas(Program.width, Program.height);
+        Shapes.GentleSlopeR(40, 8);
+        Shapes.Platform(70, 19);
+        Shapes.Platform(100, 19);
+        Shapes.Platform(130, 19);
+        Shapes.SteepSlopeL(165, 25);
+        Shapes.Platform(128, 35);
+        Shapes.Platform(115, 45);
+        Shapes.Platform(88, 55);
+        Shapes.Platform(135, 55);
+        
+    }
+
+    public void Clear(int width, int height){
+         //clear terminal and add floors and walls represented by hyphens
+        for (int i = 0; i < width - 1; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                
+                Console.SetCursorPosition(i,j);
+                Console.Write(' ');
+                Program.grid[i,j] = ' ';
+                
+            }
+        }
+        Console.SetCursorPosition(0,0);
     }
 }
